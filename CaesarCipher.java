25bd4224230864cc48faaefc9e60c3c38184c872
xhannyb <shannonbulimo@gmail.com>
2022-04-26 11:15:03 +0300

    encryption

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -11,3 +11,16 @@ public class Solution { //to keep track of index
       char replaceVal = aplha.charAt(keyVal);
       cipherText += replaceVal;
     }
+    return cipherText;
+} public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String message = new String();
+    int key = 0;
+    System.out.print("Enter the String for Encryption:");
+    message = sc.next();
+
+    System.out.println("\n\nEnter Shift Key:");
+    key = sc.nextInt();
+    System.out.println("\nEncrpyted msg:" + encrypt(message, key));
+  } //main method ends
+} //Solution Class End
