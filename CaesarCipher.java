6266ba68321675861fa3aa9944051d0ce0822fe4
xhannyb <shannonbulimo@gmail.com>
2022-04-26 11:13:06 +0300

    charposition

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -2,3 +2,12 @@ import java.io.*;
 import java.util.*;
 public class Solution { //to keep track of index
   public static final String alpha = "abcdefghijklmnopqrstuvwxyz";
+  public static String encrypt(String message, int shiftKey) {
+    message = message.toLowerCase();
+    String cipherText = "";
+    for (int ii = 0; ii < message.length(); ii++) {
+      int charPosition = aplha.indexOf(message.charAt(ii));
+      int keyVal = (shiftKey + charPosition) % 26;
+      char replaceVal = aplha.charAt(keyVal);
+      cipherText += replaceVal;
+    }
